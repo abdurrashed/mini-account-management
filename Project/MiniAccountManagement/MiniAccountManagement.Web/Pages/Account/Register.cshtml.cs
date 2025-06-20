bb345506d53d658d8649058c42a28d3f5c6f1029
abdurrashed <rashedroni1201@gmail.com>
2025-06-19 23:46:31 -0700

    Register page design completed and registermodel class updated for registration  also loginpartial view added in shared folder

--- Project/MiniAccountManagement/MiniAccountManagement.Web/Pages/Account/Register.cshtml.cs
+++ Project/MiniAccountManagement/MiniAccountManagement.Web/Pages/Account/Register.cshtml.cs
@@ -1,12 +1,83 @@
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MiniAccountManagement.Web.Infrastructure.Identity;
 
 namespace MiniAccountManagement.Web.Pages.Account
 {
     public class RegisterModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public RegisterModel(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            RoleManager<ApplicationRole> roleManager)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; } = new();
+
+        public class InputModel
+        {
+            [Required]
+            public string UserName { get; set; } = "";
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; } = "";
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; } = "";
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Compare("Password")]
+            public string ConfirmPassword { get; set; } = "";
+
+            [Required]
+            public string Role { get; set; } = "Viewer";
+        }
+
+      
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var user = new ApplicationUser
+            {
+                UserName = Input.UserName,
+                Email = Input.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, Input.Password);
+
+            if (result.Succeeded)
+            {
+                if (!await _roleManager.RoleExistsAsync(Input.Role))
+                {
+                    await _roleManager.CreateAsync(new ApplicationRole { Name = Input.Role });
+                }
+
+                await _userManager.AddToRoleAsync(user, Input.Role);
+
+                await _signInManager.SignInAsync(user, isPersistent: false);
+
+                return RedirectToPage("/Index");
+            }
+
+            foreach (var error in result.Errors)
+                ModelState.AddModelError(string.Empty, error.Description);
+
+            return Page();
         }
     }
 }
