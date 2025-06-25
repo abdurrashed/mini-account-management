fa234504da36c31d7bdc45bdf880b6834a1fc896
abdurrashed <rashedroni1201@gmail.com>
2025-06-25 11:40:19 +0600

    created voucher and voucherEntry entities

---
+++ Project/MiniAccountManagement/MiniAccountManagement.Web.Domain/Entities/Voucher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniAccountManagement.Web.Domain.Entities
+{
+    public class Voucher
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public string ReferenceNo { get; set; }
+        public string Type { get; set; }
+        public List<VoucherEntry> Entries { get; set; } = new();
+
+    }
+}
