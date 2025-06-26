81c8cd5286df21c63d1fe27887d68bf6a68b2924
abdurrashed <rashedroni1201@gmail.com>
2025-06-26 17:12:52 +0600

    added UserRepository and UserManager and rolemanager injection in userrepository constructor , updated GellUsersWithRolesasyc , Iuserrepository injection in Userservice and update getallUserasync

---
+++ Project/MiniAccountManagement/MiniAccountManagement.Web.Application/Services/UserService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiniAccountManagement.Web.Domain.DTOS;
+using MiniAccountManagement.Web.Domain.Repositories;
+using MiniAccountManagement.Web.Domain.Services;
+
+namespace MiniAccountManagement.Web.Application.Services
+{
+    public class UserService : IUserService
+    {
+
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public Task<bool> CreateUserAsync(UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteUserAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserWithRolesDto>> GetAllUsersAsync()
+        {
+            return _userRepository.GetAllUsersWithRolesAsync();
+        }
+
+        public Task<UserDto?> GetUserByIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateUserAsync(UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Project/MiniAccountManagement/MiniAccountManagement.Web.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MiniAccountManagement.Web.Domain.DTOS;
+using MiniAccountManagement.Web.Domain.Repositories;
+using MiniAccountManagement.Web.Infrastructure.Identity;
+
+namespace MiniAccountManagement.Web.Infrastructure.Repositories
+{
+    public class UserRepository :IUserRepository
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public UserRepository(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public Task<bool> CreateUserAsync(UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteUserAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<UserWithRolesDto>> GetAllUsersWithRolesAsync()
+        {
+            var users = await _userManager.Users.ToListAsync();
+
+            var list = new List<UserWithRolesDto>();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                list.Add(new UserWithRolesDto
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName ?? "",
+                    Email = user.Email ?? "",
+                    Roles = roles.ToList()
+                });
+            }
+
+            return list;
+        }
+
+        public Task<UserDto?> GetUserByIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateUserAsync(UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
