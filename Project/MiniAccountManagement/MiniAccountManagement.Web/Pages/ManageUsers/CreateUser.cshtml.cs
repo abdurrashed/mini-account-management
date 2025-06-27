9ea771137277910b577a0c5bd07d958d85893ae3
abdurrashed <rashedroni1201@gmail.com>
2025-06-27 18:44:05 +0600

    update createuser.cshtml thats inlcuded unique email and user name validation

--- Project/MiniAccountManagement/MiniAccountManagement.Web/Pages/ManageUsers/CreateUser.cshtml.cs
+++ Project/MiniAccountManagement/MiniAccountManagement.Web/Pages/ManageUsers/CreateUser.cshtml.cs
@@ -12,11 +12,14 @@ namespace MiniAccountManagement.Web.Pages.ManageUsers
     {
         private readonly IUserService _userService;
         private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public CreateUserModel(IUserService userService, RoleManager<ApplicationRole> roleManager)
+        public CreateUserModel(IUserService userService, RoleManager<ApplicationRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             _userService = userService;
             _roleManager = roleManager;
+            _userManager = userManager;
+           
         }
 
         [BindProperty]
@@ -60,6 +63,25 @@ namespace MiniAccountManagement.Web.Pages.ManageUsers
                 Password = Input.Password,
                 Role = Input.Role.Trim()
             };
+
+            bool hasError = false;
+
+            var existingUser = await _userManager.FindByEmailAsync(userDto.Email);
+            if (existingUser != null)
+            {
+                ModelState.AddModelError("Input.Email", "This email is already registered.");
+                hasError = true;
+            }
+
+            var existingUsername = await _userManager.FindByNameAsync(userDto.UserName);
+            if (existingUsername != null)
+            {
+                ModelState.AddModelError("Input.UserName", "This username is already taken.");
+                hasError = true;
+            }
+
+            if (hasError)
+                return Page();
 
             var result = await _userService.CreateUserAsync(userDto);
             if (!result)
