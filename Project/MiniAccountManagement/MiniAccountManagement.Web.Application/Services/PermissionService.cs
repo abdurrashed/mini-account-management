b600c489fd2f86fa7e00ae796f5d05201ff15210
abdurrashed <rashedroni1201@gmail.com>
2025-06-28 11:31:16 +0600

    created permissionservice and IpermissionService

---
+++ Project/MiniAccountManagement/MiniAccountManagement.Web.Application/Services/PermissionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiniAccountManagement.Web.Domain.Services;
+
+namespace MiniAccountManagement.Web.Application.Services
+{
+    public class PermissionService : IPermissionService
+    {
+        public Task<bool> HasPermissionAsync(string userId, string moduleName, string action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Project/MiniAccountManagement/MiniAccountManagement.Web.Domain/Services/IPermissionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniAccountManagement.Web.Domain.Services
+{
+    public interface IPermissionService
+    {
+        Task<bool> HasPermissionAsync(string userId, string moduleName, string action);
+
+    }
+}
